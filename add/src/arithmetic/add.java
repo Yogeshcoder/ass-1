bb61a83a04ddfe6129ac779c84f52f84a80a1e82
Yogesh <Yogesh@DESKTOP-BS955DP>
2021-07-28 18:42:46 +0530

    ass 1

---
+++ add/src/arithmetic/add.java
@@ -0,0 +1,9 @@
+package arithmetic;
+	class add{
+		public static void main(String[]args)
+		{
+			math.square obj=new math.square();
+			obj.square();
+			obj.cube();
+		}
+}
---
+++ add/src/math/square.java
@@ -0,0 +1,13 @@
+package math;
+
+public class square {
+public  void square() 
+{
+
+	System.out.println(3*3);
+}
+public  void cube()
+{
+	System.out.println(3*3*3);
+}
+}
